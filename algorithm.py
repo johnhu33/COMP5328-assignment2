8bb137fecbaf5720eadae5087f8c8296d383be3d
johnhu33 <74128188+johnhu33@users.noreply.github.com>
2020-11-08 19:21:30 +1100

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -12,7 +12,90 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
+from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Module, Softmax, BatchNorm2d, Dropout
+from torch.optim import Adam, SGD
+import torchvision
 
+class Bottleneck(nn.Module):
+  expansion = 4
+  
+  def __init__(self, in_dim, out_dim, identity_shortcut = None, stride=1):
+    super(Bottleneck, self).__init__()
+    self.conv1 = nn.Conv2d(in_dim, out_dim, kernel_size=1)
+    self.bn1 = nn.BatchNorm2d(out_dim)
+    self.conv2 = nn.Conv2d(out_dim, out_dim, kernel_size=3,stride=stride, padding=1)
+    self.bn2 = nn.BatchNorm2d(out_dim)
+    self.conv3 = nn.Conv2d(out_dim, self.expansion*out_dim, kernel_size=1)
+    self.bn3 = nn.BatchNorm2d(self.expansion*out_dim)
+    self.relu = nn.ReLU()
+    self.identity_shortcut = identity_shortcut
+  
+  def forward(self, x):
+    identity = x
+    out = self.conv1(x)
+    out = self.bn1(out)
+    out = self.relu(out)
+    out = self.conv2(out)
+    out = self.bn2(out)
+    out = self.relu(out)
+    out = self.conv3(out)
+    out = self.bn3(out)
+
+    if self.identity_shortcut is not None:
+      identity = self.identity_shortcut(identity)
+
+    out += identity
+    out = self.relu(out)
+    return out
+
+class ResNet(nn.Module):
+  def __init__(self, block, num_blocks, num_classes,image_channels):
+    super(ResNet, self).__init__()
+    self.in_planes = 64
+    self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7,stride=1, padding=3, bias=False)
+    self.bn1 = nn.BatchNorm2d(64)
+    self.relu = nn.ReLU()
+    self.maxpool = nn.MaxPool2d(kernel_size=3,stride=2,padding =1)
+    self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+    self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+    self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+    self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+    self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+    self.fc = nn.Linear(512*4,num_classes)
+
+  def _make_layer(self, block, out_dim, num_blocks, stride):
+    identity_shortcut = None
+    layers = []
+    if stride != 1 or self.in_dim != out_dim*4:
+      identity_shortcut = nn.Sequential(nn.Conv2d(self.in_dim,out_dim*4,kernel_size=1,stride=stride), nn.BatchNorm2d(out_dim*4))
+    layers.append(block(self.in_dim, out_dim, identity_shortcut, stride))
+    self.in_dim = out_dim*4
+
+    for i in range (num_blocks -1):
+      layers.append(block(self.in_dim,out_dim))
+      
+    return nn.Sequential(*layers)
+
+
+  def forward(self, x):
+      out = torch.relu(self.bn1(self.conv1(x)))
+      out = self.layer1(out)
+      out = self.layer2(out)
+      out = self.layer3(out)
+      out = self.layer4(out)
+      out = self.avgpool(out)
+      out = out.view(out.size(0), -1)
+      out = self.fc(out)
+      return out
+
+
+def Res50 (num_layers, num_classes = 3, image_channels =3):
+  return ResNet(Bottleneck, [3,4,6,3], image_channels, num_classes)
+def Res101 (num_layers, num_classes = 3, image_channels =3):
+  return ResNet(Bottleneck, [3,4,23,3], image_channels, num_classes)
+def Res152 (num_layers, num_classes = 3, image_channels =3):
+  return ResNet(Bottleneck, [3,8,36,3], image_channels, num_classes)
 
 class Backward:
     def __init__(self, model):
@@ -168,6 +251,9 @@ DATA = {
     'FashionMNIST0.6': [[0.4, 0.3, 0.3], [0.3, 0.4, 0.3], [0.3, 0.3, 0.4]],
 }
 MODEL = {
+    'Forward_Res50':Forward(NeuralNet(Res50))
+    'Forward_Res101':Forward(NeuralNet(Res101))
+    'Forward_Res152':Forward(NeuralNet(Res152))
     'forward_linear': Forward(linear),
     'backward_linear': Backward(NeuralNet(linear)),
     'forward_three_layer': Forward(three_layer),
